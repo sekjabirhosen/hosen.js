1634b557d9308858d89e9345573578c9560c1098
sekjabirhosen <162603600+sekjabirhosen@users.noreply.github.com>
2024-07-29 04:41:15 +0000

    container config done

---
+++ .github/basic.js
@@ -0,0 +1,13 @@
+const accountId=12345;
+let accountEmail="jabir@gmail.com"
+var accountPassword="1234567"
+accountCity="jaipur"
+
+// accountId=45678
+console.log("accountId")
+accountEmail="hussain@gmail.com"
+accountPassword="098765"
+accountCity="Bangaluru"
+let accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
