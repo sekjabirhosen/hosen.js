226396b7fd92cf357dc108bb3c184b9080c8d150
sekjabirhosen <162603600+sekjabirhosen@users.noreply.github.com>
2024-08-21 16:11:26 +0000

    data type in js

---
+++ .github/03script.js
@@ -0,0 +1,15 @@
+let score="33wbc";
+console.log(typeof(score));
+console.log(typeof score);
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(+true)
+console.log(+"");
+//postfix prfix
+let num1,num2,num3;
+num1=num2=num3=2+2;
+let gamecounter=100;
+gamecounter++;
+console.log(gamecounter);
+// ++gamecounter
---
+++ .github/04script.js
@@ -0,0 +1,20 @@
+// const isLoggedIn=false;
+// const OutsideTemp=null;
+// let useEmail;
+// const Id=Symbol('123');
+// const anothorId=Symbol('123');
+// console.log(Id===anothorId);
+// console.log(Id==anothorId);
+const bidNumber=234665467n;
+const Heros=["shaktiman","Ironman","batman"];
+let myobj={
+    Name:"jabir",
+    age:23,
+
+}
+console.log(Heros);
+console.log(myobj)
+ const myfunction=function(){
+    console.log("hello world")
+ }
+ console.log(typeof(myfunction));
